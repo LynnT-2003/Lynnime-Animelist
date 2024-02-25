fafccbfc325d4e7b7bacf5176263b1cc43dd0c4b
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-02-26 00:19:59 +0700

    Completed Discover Screen with Anime API

--- app/src/main/java/com/example/lynnime/fragments/ExploreFragment.kt
+++ app/src/main/java/com/example/lynnime/fragments/ExploreFragment.kt
@@ -1,5 +1,6 @@
 package com.example.lynnime.fragments
 
+import JikanAnimeModel
 import android.graphics.Movie
 import android.os.Bundle
 import android.util.Log
@@ -15,17 +16,20 @@ import com.example.lynnime.R
 import com.example.lynnime.api.RetrofitClient
 import com.example.lynnime.databinding.FragmentExploreBinding
 import com.example.lynnime.models.HorrorMovieData
+import com.example.lynnime.utils.AnimeAdapter
 import com.example.lynnime.utils.HorrorMovieAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ExploreFragment : Fragment() {
 
     private lateinit var binding: FragmentExploreBinding
+    private lateinit var animeAdapter: AnimeAdapter
+    private var animeList: MutableList<JikanAnimeModel.Anime> = mutableListOf()
 
     private lateinit var navController: NavController
 
     private lateinit var horrorMovieAdapter: HorrorMovieAdapter
-    private var horrorMoviesList: MutableList<HorrorMovieData> = mutableListOf()
+//    private var horrorMoviesList: MutableList<HorrorMovieData> = mutableListOf()
 
     private var currentIndex = 0;
 
@@ -40,8 +44,17 @@ class ExploreFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        horrorMovieAdapter = HorrorMovieAdapter(horrorMoviesList) {}
+//        horrorMovieAdapter = HorrorMovieAdapter(horrorMoviesList) {}
+        animeAdapter = AnimeAdapter(animeList) {}
+
         super.onViewCreated(view, savedInstanceState)
+        fetchAnimeData()
+        view.setOnClickListener {
+            if (animeList.isNotEmpty()) {
+                currentIndex = (currentIndex + 1) % animeList.size
+                updateUI(animeList[currentIndex])
+            }
+        }
 //        getHorrorMovies()
 //        view.setOnClickListener {
 //            if (horrorMoviesList.isNotEmpty()) {
@@ -69,12 +82,36 @@ class ExploreFragment : Fragment() {
 //        })
 //    }
 
-    fun updateUI(movie: HorrorMovieData) {
-        binding.title.text = movie.title
-//        binding.title2.text = movie.description
-        // Load image using Glide or similar library
-        Glide.with(this).load(movie.posterURL).into(binding.cover)
-        // Update tags, etc.
+    private fun fetchAnimeData() {
+        RetrofitClient.instance.getCurrentSeasonAnime().enqueue(object : retrofit2.Callback<JikanAnimeModel> { // Use the correct callback type
+            override fun onResponse(call: retrofit2.Call<JikanAnimeModel>, response: retrofit2.Response<JikanAnimeModel>) {
+                if (response.isSuccessful && response.body() != null) {
+                    animeList.clear()
+                    animeList.addAll(response.body()!!.data) // Ensure your API's response structure is correctly navigated
+                    animeAdapter.notifyDataSetChanged()
+                } else {
+                    Log.e("HomeFragment", "Error fetching anime data")
+                }
+            }
+
+            override fun onFailure(call: retrofit2.Call<JikanAnimeModel>, t: Throwable) {
+                Log.e("HomeFragment", "Failure: ${t.message}")
+            }
+        })
+    }
+
+//    fun updateUI(movie: HorrorMovieData) {
+//        binding.title.text = movie.title
+////        binding.title2.text = movie.description
+//        // Load image using Glide or similar library
+//        Glide.with(this).load(movie.posterURL).into(binding.cover)
+//        // Update tags, etc.
+//    }
+
+    private fun updateUI(anime: JikanAnimeModel.Anime) {
+        binding.title.text = anime.titleEnglish
+        binding.title2.text = anime.synopsis
+        Glide.with(this).load(anime.images.jpg.largeImageUrl).into(binding.cover)
     }
 
 }
