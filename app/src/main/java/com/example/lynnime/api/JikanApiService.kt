1f31f5aa0624c5c7c1eeff70d54615318462e329
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-02-26 00:59:37 +0700

    Added new API routes, and adjusted footer paddings.

--- app/src/main/java/com/example/lynnime/api/JikanApiService.kt
+++ app/src/main/java/com/example/lynnime/api/JikanApiService.kt
@@ -7,4 +7,10 @@ import retrofit2.http.GET
 interface JikanApiService {
     @GET("seasons/now")
     fun getCurrentSeasonAnime(): Call<JikanAnimeModel>
+
+    @GET("top/anime")
+    fun getTopAnime(): Call<JikanAnimeModel>
+
+    @GET("seasons/upcoming")
+    fun getUpcomingSeasonAnime(): Call<JikanAnimeModel>
 }
